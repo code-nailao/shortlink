8078a30de9ecf19e255dcf3dd040d17fe6f6a361
nailao <8975507+liangxi-qiqi@user.noreply.gitee.com>
2024-05-28 20:52:38 +0800

    feat:新增包目录

---
+++ admin/src/main/java/org/nailao/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,15 @@
+package org.nailao.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yang
+ * @date 2024/5/28 20:45
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
