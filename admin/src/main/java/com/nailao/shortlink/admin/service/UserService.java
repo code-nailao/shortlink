ef28063bcdc72fcab0f16dbc1d4a002cf6e8a77d
nailao <8975507+liangxi-qiqi@user.noreply.gitee.com>
2024-05-29 15:46:25 +0800

    feat: 开发短链接后管系统用户注册相关功能

--- admin/src/main/java/com/nailao/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nailao/shortlink/admin/service/UserService.java
@@ -2,6 +2,7 @@ package com.nailao.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nailao.shortlink.admin.dao.entity.UserDO;
+import com.nailao.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.nailao.shortlink.admin.dto.resp.UserRespDTO;
 
 /**
@@ -19,5 +20,20 @@ public interface UserService extends IService<UserDO> {
      * @return 用户返回实体
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     * 查询用户名是否存在
+     *
+     * @param username 用户名
+     * @return 用户名存在返回 True，不存在返回 False
+     */
+    Boolean hasUsername(String username);
+
+    /**
+     * 注册用户
+     *
+     * @param requestParam 注册用户请求参数
+     */
+    void register(UserRegisterReqDTO requestParam);
 
 }
--- admin/src/main/java/com/nailao/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/nailao/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,15 +1,26 @@
 package com.nailao.shortlink.admin.service.impl;
 
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nailao.shortlink.admin.common.convention.exception.ClientException;
+import com.nailao.shortlink.admin.common.convention.exception.ServiceException;
+import com.nailao.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.nailao.shortlink.admin.dao.entity.UserDO;
-import com.nailao.shortlink.admin.dto.resp.UserRespDTO;
 import com.nailao.shortlink.admin.dao.mapper.UserMapper;
+import com.nailao.shortlink.admin.dto.req.UserRegisterReqDTO;
+import com.nailao.shortlink.admin.dto.resp.UserRespDTO;
 import com.nailao.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBloomFilter;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.nailao.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NAME_EXIST;
+import static com.nailao.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SAVE_ERROR;
 
 /**
  * 用户接口实现层
@@ -18,14 +29,40 @@ import org.springframework.stereotype.Service;
  * @date 2024/5/28 21:42
  */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
                 .eq(UserDO::getUsername, username);
         UserDO userDO = baseMapper.selectOne(queryWrapper);
+        if (userDO == null) {
+            throw new ServiceException(UserErrorCodeEnum.USER_NULL);
+        }
         UserRespDTO result = new UserRespDTO();
         BeanUtils.copyProperties(userDO, result);
         return result;
+    }
+
+    @Override
+    public Boolean hasUsername(String username) {
+        return !userRegisterCachePenetrationBloomFilter.contains(username);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void register(UserRegisterReqDTO requestParam) {
+        if (!hasUsername(requestParam.getUsername())) {
+            throw new ClientException(USER_NAME_EXIST);
+        }
+
+        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+        if (inserted < 1) {
+            throw new ClientException(USER_SAVE_ERROR);
+        }
+        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 }
