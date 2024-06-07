de70b4ea09864038777c53c49f3b5765b9e01c30
code-nailao <596268641@qq.com>
2024-06-07 19:44:50 +0800

    feat: 开发短链接后管分组新增功能

--- admin/src/main/java/com/nailao/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/nailao/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -9,6 +9,7 @@ import com.nailao.shortlink.admin.common.convention.exception.ClientException;
 import com.nailao.shortlink.admin.dao.entity.GroupDO;
 import com.nailao.shortlink.admin.dao.mapper.GroupMapper;
 import com.nailao.shortlink.admin.service.GroupService;
+import com.nailao.shortlink.admin.toolkit.RandomGenerator;
 import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
@@ -38,7 +39,25 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public void saveGroup(String groupName) {
+        String gid;
+        do {
+            gid = RandomGenerator.generateRandom();
+        } while (!hasGid(gid));
+        GroupDO groupDO = GroupDO.builder()
+                .gid(gid)
+                .name(groupName)
+                .sortOrder(0)
+                .build();
+        baseMapper.insert(groupDO);
+    }
 
+    private boolean hasGid(String gid) {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getGid, gid)
+                //todo 设置用户名
+                .eq(GroupDO::getUsername, null);
+        GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
+        return hasGroupFlag == null;
     }
 
 }
