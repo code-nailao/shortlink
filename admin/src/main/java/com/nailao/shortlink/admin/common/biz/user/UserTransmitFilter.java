5566d048f7dd276e5a9ec090f70f792409c23a87
code-nailao <596268641@qq.com>
2024-06-11 19:33:51 +0800

    feat: 开发短链接系统common

--- admin/src/main/java/com/nailao/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/nailao/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,13 +1,21 @@
 package com.nailao.shortlink.admin.common.biz.user;
 
 import cn.hutool.core.util.StrUtil;
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
+import com.nailao.shortlink.admin.common.convention.exception.ClientException;
+import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+import static com.nailao.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 /**
  * 用户信息传输过滤器
@@ -16,18 +24,35 @@ import lombok.SneakyThrows;
  * @date 2024/6/10 13:14
  */
 @RequiredArgsConstructor
+@NoArgsConstructor(force = true)
 public class UserTransmitFilter implements Filter {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/v1/user/has-username"
+    );
 
     @SneakyThrows
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String username = httpServletRequest.getHeader("username");
-        if (StrUtil.isNotBlank(username)) {
-            String userId = httpServletRequest.getHeader("userId");
-            String realName = httpServletRequest.getHeader("realName");
-            UserInfoDTO userInfoDTO = new UserInfoDTO(userId, username, realName);
-            UserContext.setUser(userInfoDTO);
+        String requestURI = httpServletRequest.getRequestURI();
+        if (!IGNORE_URI.contains(requestURI)) {
+            String method = httpServletRequest.getMethod();
+            if (!(Objects.equals(requestURI, "/api/short-link/admin/v1/user") && Objects.equals(method, "POST"))) {
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    throw new ClientException(USER_TOKEN_FAIL);
+                }
+                Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                if (userInfoJsonStr != null) {
+                    UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                    UserContext.setUser(userInfoDTO);
+                }
+            }
         }
         try {
             filterChain.doFilter(servletRequest, servletResponse);
