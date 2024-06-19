66bc293647b8af3b45eda3fb6488bf0b6178cdd8
code-nailao <596268641@qq.com>
2024-06-19 14:16:05 +0800

    feat: 提交MySQL数据库分页插件

---
+++ admin/src/main/java/com/nailao/shortlink/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,29 @@
+package com.nailao.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 数据库持久层配置类
+ *
+ * @author nailao
+ * @date 2024/6/19 14:14
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptorByAdmin() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
