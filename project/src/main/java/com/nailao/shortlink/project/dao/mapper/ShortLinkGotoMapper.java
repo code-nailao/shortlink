3134bba08855acdf0222527f0724e6817ccc7d81
code-nailao <596268641@qq.com>
2024-07-14 16:56:56 +0800

    feat: 开发短链接跳转功能

---
+++ project/src/main/java/com/nailao/shortlink/project/dao/mapper/ShortLinkGotoMapper.java
@@ -0,0 +1,13 @@
+package com.nailao.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nailao.shortlink.project.dao.entity.ShortLinkGotoDO;
+
+/**
+ * 短链接跳转持久层
+ *
+ * @author nailao
+ * @date 2024/7/14 11:01
+ */
+public interface ShortLinkGotoMapper extends BaseMapper<ShortLinkGotoDO> {
+}
