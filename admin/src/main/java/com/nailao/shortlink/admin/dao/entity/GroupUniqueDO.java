2b20f1db51475a81f641ab76b31ed03363df1af9
code-nailao <596268641@qq.com>
2024-07-14 16:41:20 +0800

    feat: 开发短链接实体

---
+++ admin/src/main/java/com/nailao/shortlink/admin/dao/entity/GroupUniqueDO.java
@@ -0,0 +1,31 @@
+package com.nailao.shortlink.admin.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 短链接分组唯一路由实体
+ *
+ * @author nailao
+ * @date 2024/7/14 14:20
+ */
+@Data
+@TableName("t_group_unique")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupUniqueDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+}
---
+++ project/src/main/java/com/nailao/shortlink/project/dao/entity/GroupDO.java
@@ -0,0 +1,47 @@
+package com.nailao.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.nailao.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 短链接分组实体
+ *
+ * @author nailao
+ * @date 2024/7/14 10:39
+ */
+@Data
+@TableName("t_group")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 分组名称
+     */
+    private String name;
+
+    /**
+     * 创建分组用户名
+     */
+    private String username;
+
+    /**
+     * 分组排序
+     */
+    private Integer sortOrder;
+}
