8eada272a9d29349f8da7e83a7192c7f944d986e
code-nailao <596268641@qq.com>
2024-07-14 16:57:41 +0800

    feat: 开发短链接分组持久层

---
+++ project/src/main/java/com/nailao/shortlink/project/dao/mapper/LinkGroupMapper.java
@@ -0,0 +1,13 @@
+package com.nailao.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nailao.shortlink.project.dao.entity.GroupDO;
+
+/**
+ * 短链接分组持久层
+ *
+ * @author nailao
+ * @date 2024/7/14 11:01
+ */
+public interface LinkGroupMapper extends BaseMapper<GroupDO> {
+}
