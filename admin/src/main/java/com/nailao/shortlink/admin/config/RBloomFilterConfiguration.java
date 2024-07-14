217fa810ffc7c8e870870622ca918e503ffffbaf
code-nailao <596268641@qq.com>
2024-07-14 17:04:15 +0800

    feat：开发短链接防止分组标识注册查询数据库的布隆过滤器

--- admin/src/main/java/com/nailao/shortlink/admin/config/RBloomFilterConfiguration.java
+++ admin/src/main/java/com/nailao/shortlink/admin/config/RBloomFilterConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @author nailao
  * @date 2024/5/29 14:52
  */
-@Configuration
+@Configuration(value = "rBloomFilterConfigurationByAdmin")
 public class RBloomFilterConfiguration {
 
     /**
@@ -21,6 +21,16 @@ public class RBloomFilterConfiguration {
     public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
         RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
         cachePenetrationBloomFilter.tryInit(100000000L, 0.001);
+        return cachePenetrationBloomFilter;
+    }
+
+    /**
+     * 防止分组标识注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> gidRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("gidRegisterCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(200000000L, 0.001);
         return cachePenetrationBloomFilter;
     }
 }
