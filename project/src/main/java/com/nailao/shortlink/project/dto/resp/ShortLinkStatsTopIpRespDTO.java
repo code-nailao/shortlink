11eb63f50b22bf57fcfc2a66ee17170d5522f547
code-nailao <596268641@qq.com>
2024-07-14 16:32:36 +0800

    feat: 开发短链接高频访问IP监控响应参数

---
+++ project/src/main/java/com/nailao/shortlink/project/dto/resp/ShortLinkStatsTopIpRespDTO.java
@@ -0,0 +1,29 @@
+package com.nailao.shortlink.project.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 短链接高频访问IP监控响应参数
+ *
+ * @author nailao
+ * @date 2024/7/14 10:56
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkStatsTopIpRespDTO {
+
+    /**
+     * 统计
+     */
+    private Integer cnt;
+
+    /**
+     * IP
+     */
+    private String ip;
+}
